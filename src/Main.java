a5681b6e1df1861a3a7518bfef0cb27e2545a99d
Nero-He <hongzhe.he@my.jcu.edu.au>
2021-12-06 13:39:08 +0800

    created some output words for main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+
 public class Main {
+    public static void main(String[] args) {
+
+        // I guess I should print out something as a main menu
+        Scanner simController = new Scanner(System.in);
+        System.out.println("Object Creation: \n——————————————————————");
+        System.out.println("Roads:");
+        System.out.println("\nRoads:");
+        System.out.println("\nCars:");
+        System.out.println("\nTraffic Lights:");
+        System.out.println("Settings:");
+    }
 }
